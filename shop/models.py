cb143e7b614b76d6c8225fac92d2120bd1477888
YangGa Eun <gaeunyang0224@gmail.com>
2017-02-16 11:59:28 +0900

    s
    s

--- shop/models.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import User
-from django.db import models
-
-
-class Post(models.Model):
-    user = models.ForeignKey(User, related_name='shop_post_set')
--- webtoon/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from webtoon import views
-
-urlpatterns = [
-    url(r'^$', views.webtoon_list),
-    url(r'^(?P<id>\d+)/$', views.webtoon_detail),
-]
-
